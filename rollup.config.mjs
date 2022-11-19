031cc017556c19a9facb3626973d98e2fbbb5b31
AkaraChen <85140972+AkaraChen@users.noreply.github.com>
2022-11-19 20:45:29 +0800

    🐳 chore: drop autoprefixer

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,64 +1,64 @@
-import { defineConfig } from "rollup";
-import glob from "glob";
-import fs from "node:fs";
-import path from "node:path";
-import resolve from "@rollup/plugin-node-resolve";
-import esbuild from "rollup-plugin-esbuild";
-import alias from "@rollup/plugin-alias";
-import litCss from "rollup-plugin-lit-css";
-import del from "rollup-plugin-delete";
-import MinifyHTML from "rollup-plugin-minify-html-literals";
-import postcss from "postcss";
-import postcssNesting from "postcss-nesting";
-import syntax from "postcss-less";
-import autoprefixer from "autoprefixer";
-import { Processor } from "windicss/lib";
-import { HTMLParser } from "windicss/utils/parser";
+import {defineConfig} from 'rollup';
+import glob from 'glob';
+import fs from 'node:fs';
+import path from 'node:path';
+import resolve from '@rollup/plugin-node-resolve';
+import esbuild from 'rollup-plugin-esbuild';
+import alias from '@rollup/plugin-alias';
+import litCss from 'rollup-plugin-lit-css';
+import del from 'rollup-plugin-delete';
+import MinifyHTML from 'rollup-plugin-minify-html-literals';
+import postcss from 'postcss';
+import postcssNesting from 'postcss-nesting';
+import syntax from 'postcss-less';
+import {Processor} from 'windicss/lib';
+import {HTMLParser} from 'windicss/utils/parser';
+import filesize from 'rollup-plugin-filesize';
 
-const processor = postcss(postcssNesting(), autoprefixer());
+const processor = postcss(postcssNesting());
 
 export default defineConfig({
-    input: glob.GlobSync("./src/*.ts").found,
+    input: glob.GlobSync('./src/*.ts').found,
     output: {
-        dir: "dist",
-        chunkFileNames: "chunk/[hash].js",
+        dir: 'dist',
+        chunkFileNames: 'chunk/[hash].js',
         compact: true,
     },
     plugins: [
         litCss({
             include: /\.less$/i,
-            transform: (css, { filePath }) => {
+            transform: (css, {filePath}) => {
                 const fileName = filePath
-                    .replaceAll("/", "\\")
-                    .split("\\")
+                    .replaceAll('/', '\\')
+                    .split('\\')
                     .at(-1);
                 const template = path.resolve(
-                    "./src",
-                    fileName.split(".")[0] + ".ts"
+                    './src',
+                    fileName.split('.')[0] + '.ts'
                 );
                 const content = fs.readFileSync(template).toString();
                 const windiProcessor = new Processor();
                 const htmlClasses = new HTMLParser(content)
                     .parseClasses()
-                    .map((index) => index.result)
-                    .join(" ");
-                const interpretedSheet =
-                    windiProcessor.interpret(htmlClasses).styleSheet;
+                    .map(index => index.result)
+                    .join(' ');
+                const interpretedSheet = windiProcessor.interpret(htmlClasses).styleSheet;
                 const styles = interpretedSheet.build();
 
                 const base = fs
-                    .readFileSync(path.resolve("./style/base.less"))
+                    .readFileSync(path.resolve('./style/base.less'))
                     .toString();
                 return processor.process(base + css + styles, {
                     from: filePath,
-                    syntax,
+                    syntax
                 }).css;
-            },
+            }
         }),
         MinifyHTML.default(),
-        esbuild({ minify: true }),
+        esbuild({minify: true}),
         resolve(),
-        alias({ entries: { "@": "./src", "#": "./style" } }),
-        del({ targets: "dist/*" }),
-    ],
+        alias({entries: {'@': './src', '#': './style'}}),
+        del({targets: 'dist/*'}),
+        filesize()
+    ]
 });
