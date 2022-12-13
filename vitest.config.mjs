e5f937eada19a18effc19502c74fcec735aa568e
AkaraChen <85140972+AkaraChen@users.noreply.github.com>
2022-12-13 14:33:56 +0800

    Revert "test: init"
    This reverts commit 32716a85fe996bcf8eabcd82670000468c63efb6.

--- vitest.config.mjs
+++
@@ -1,6 +0,0 @@
-import {litSsrPlugin} from '@lit-labs/testing/web-test-runner-ssr-plugin.js';
-import {esbuildPlugin} from '@web/dev-server-esbuild';
-
-export default {
-    plugins: [esbuildPlugin({ts: true}), litSsrPlugin()]
-};
