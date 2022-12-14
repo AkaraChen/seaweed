f8d2c568a98b8850c79634c8ded261cfb58664e9
AkaraChen <85140972+AkaraChen@users.noreply.github.com>
2022-12-14 18:15:57 +0800

    fix: ts error

--- packages/shared/test.ts
+++ packages/shared/test.ts
@@ -1,8 +1,7 @@
 import {fixture} from '@open-wc/testing';
-import {LitElement} from 'lit';
 import {expect} from 'vitest';
 
-export const render = async (tagName: string, constructor: LitElement) => {
+export const render = async (tagName: string, constructor: any) => {
     const element = await fixture(`<${tagName}></${tagName}>`) as typeof constructor;
     expect(element.shadowRoot).not.toBeNull();
     expect(element instanceof HTMLElement).toBeTruthy();
