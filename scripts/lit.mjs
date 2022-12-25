40f1434705609152aad18529b29fb1c6453eee96
AkaraChen <85140972+AkaraChen@users.noreply.github.com>
2022-12-25 09:10:47 +0800

    feat: dark mode for collapse

--- scripts/lit.mjs
+++ scripts/lit.mjs
@@ -1,4 +1,4 @@
-import {litCss} from 'rollup-plugin-lit-css';
+import { litCss } from 'rollup-plugin-lit-css';
 import postcss from 'postcss';
 import postcssNesting from 'postcss-nesting';
 import syntax from 'postcss-less';
@@ -6,13 +6,12 @@ import fs from 'node:fs';
 import path from 'node:path';
 
 const base = fs
-    .readFileSync(
-        path.resolve('./packages/shared/base.less')
-    ).toString();
+    .readFileSync(path.resolve('./packages/shared/base.less'))
+    .toString();
 
 export const litcssPlugin = litCss({
     include: /\.less$/i,
-    transform: (css, {filePath: from}) => {
+    transform: (css, { filePath: from }) => {
         const processor = postcss(postcssNesting());
         return processor.process(base + css, {
             from,
