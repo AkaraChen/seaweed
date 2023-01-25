e4eef868f8f0b0a66f3c324a81a91126b1744d99
AkaraChen <85140972+AkaraChen@users.noreply.github.com>
2023-01-25 19:58:16 +0800

    docs: dark mode

--- internal/dumi/.dumirc.ts
+++ internal/dumi/.dumirc.ts
@@ -1,12 +1,16 @@
 import { defineConfig } from 'dumi';
 
 export default defineConfig({
-  themeConfig: {
-    name: 'Seaweed',
+    themeConfig: {
+        name: 'Seaweed',
         nav: [
             { title: '介绍', link: '/guide/introduction' },
-            { title: '组件', link: '/components/button'}
-        ]
-  },
-  mfsu: false
+            { title: '组件', link: '/components/button' }
+        ],
+        prefersColor: {
+            default: 'auto',
+            switch: false
+        }
+    },
+    mfsu: false
 });
